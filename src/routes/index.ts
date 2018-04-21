1a536cb48421851fec3c7b81db405e02ad2d1a5b
Satit Rianpit <siteslave@msn.com>
2018-04-21 17:07:17 +0700

    add json response for default route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
-
 import * as express from 'express';
 import { Router, Request, Response } from 'express';
+import * as HttpStatus from 'http-status-codes';
 
 const router: Router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-  res.render('index', { title: 'Express' });
+  res.send({ ok: true, message: 'Welcome to RESTful api server!', code: HttpStatus.OK });
 });
 
 export default router;
