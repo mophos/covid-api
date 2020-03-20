21d11c994ccfd227af003e34a48057c13c099ed0
naphattharawat <naphattharawat@gmail.com>
2020-03-20 14:00:52 +0700

    debug false

--- src/app.ts
+++ src/app.ts
@@ -46,7 +46,7 @@ let connection: MySqlConnectionConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   multipleStatements: true,
-  debug: true
+  debug: false
 }
 
 let db = Knex({
